d036558f84cccdf22d17b497a32504a7a9938520
Sal Frisina+++++++++++++ <Sfrisina@gmail.com>
2022-04-25 17:55:23 -0400

    adding codewars 04/25/22

---
+++ cw042522.js
@@ -0,0 +1,40 @@
+// Kata Task
+
+// I have a cat and a dog.
+
+// I got them at the same time as kitten/puppy. That was humanYears years ago.
+
+// Return their respective ages now as [humanYears,catYears,dogYears]
+
+// NOTES:
+
+//     humanYears >= 1
+//     humanYears are whole numbers only
+
+// Cat Years
+
+//     15 cat years for first year
+//     +9 cat years for second year
+//     +4 cat years for each year after that
+
+// Dog Years
+
+//     15 dog years for first year
+//     +9 dog years for second year
+//     +5 dog years for each year after that
+var humanYearsCatYearsDogYears = function(humanYears) {
+    let cat
+    let dog
+    if(humanYears === 1){
+      cat = 15;
+      dog = 15;
+    }else if(humanYears === 2){
+       dog = 24;
+       cat = 24;
+    }else{
+       dog = 24 + ((humanYears - 2) * 5)
+       cat = 24 + ((humanYears - 2) * 4)
+    }
+    
+    return [humanYears,cat,dog];
+  }
