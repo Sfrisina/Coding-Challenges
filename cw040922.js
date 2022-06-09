40d86861019efca5a2a3b571b37981a16f796f3d
Sal Frisina+++++++++++++ <Sfrisina@gmail.com>
2022-06-09 10:34:47 -0400

    adding codeing challenge for 06/09/22

--- cw040922.js
+++ cw040922.js
@@ -1,20 +1,20 @@
-Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+// Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
 
-Example:
+// Example:
 
-["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
 
-function removeEveryOther(arr){
-    for(let i = 1; i < arr.length; i += 1){
-      arr.splice(i, 1);
-    }
-    return arr
-    }
+// function removeEveryOther(arr){
+//     for(let i = 1; i < arr.length; i += 1){
+//       arr.splice(i, 1);
+//     }
+//     return arr
+//     }
 
 
-    In this simple exercise, you will build a program that takes a value, integer , and returns a list of its multiples up to another value, limit . If limit is a multiple of integer, it should be included as well. There will only ever be positive integers passed into the function, not consisting of 0. The limit will always be higher than the base.
+//     In this simple exercise, you will build a program that takes a value, integer , and returns a list of its multiples up to another value, limit . If limit is a multiple of integer, it should be included as well. There will only ever be positive integers passed into the function, not consisting of 0. The limit will always be higher than the base.
 
-For example, if the parameters passed are (2, 6), the function should return [2, 4, 6] as 2, 4, and 6 are the multiples of 2 up to 6.
+// For example, if the parameters passed are (2, 6), the function should return [2, 4, 6] as 2, 4, and 6 are the multiples of 2 up to 6.
 
 function findMultiples(integer, limit) {
     let result = []
